0c74d74db59371130ad009a115104e2b81aa9ae1
mimimiㅤ ㅤ <kascha767611@gmail.com>
2025-09-21 21:44:55 +0300

    Added GetFilesDirectories method

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,1 +1,58 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{   
+
+    static (string sequencesFilePath, string commandsFilePath) GetFilesDirectories()
+    {
+        string sequencesFilePath = "";
+        string commandsFilePath = "";
+
+        string contentPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\content");
+        string[] sequencesfiles = Directory.GetFiles(contentPath, "sequences*.txt");
+
+        if (sequencesfiles.Length > 0)
+        {
+            Console.WriteLine("Choose sequences file:");
+            for (int i = 0; i < sequencesfiles.Length; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + Path.GetFileNameWithoutExtension(sequencesfiles[i]));
+            }
+
+            int fileChoise;
+            while (true)
+            {
+                Console.Write(">>> ");
+                string? input = Console.ReadLine();
+
+                if (int.TryParse(input, out fileChoise) &&
+                    fileChoise >= 1 && fileChoise <= sequencesfiles.Length)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Wrong input!");
+            }
+
+            sequencesFilePath = sequencesfiles[fileChoise - 1];
+            string filesID = Path.GetFileName(sequencesFilePath).Substring("sequences".Length);
+            commandsFilePath = Path.Combine(contentPath, "commands" + filesID);
+
+            if (!File.Exists(commandsFilePath))
+            {
+                Console.WriteLine("No commands file for this sequences file!");
+                commandsFilePath = "";
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("Not found any sequences files");
+        }
+
+        return (sequencesFilePath, commandsFilePath);
+    }
+
+    static void Main(string[] args)
+    {
+        var filesPaths = GetFilesDirectories();
+    }
+}

e90ae438e40d34de610885e0f33d63542c226579
mimimiㅤ ㅤ <kascha767611@gmail.com>
2025-09-21 15:16:20 +0300

    test commit

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{   
+    struct Sequance
+    {
+        string proteinName;
+        string organismName;
+        string proteinSequance;
+    }
+
+    static string GetFileDirectory()
+    {
+
+        return "";
+    }
+
+    static Sequance GetSequencesFileContent(string filePath)
+    {
+        Sequance sequance = new Sequance();
+
+        return sequance;
+    }
+
+    static void Main(string[] args)
+    {
+        string contentPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\content");
+        string[] files = Directory.GetFiles(contentPath, "*.txt");
+
+        foreach (string file in files)
+        {
+            Console.WriteLine(file);
+        }
+
+        Console.ReadLine();
+    }
+}

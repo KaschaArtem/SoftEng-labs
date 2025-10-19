519a8158f34e0369b34d098b93ee2f6795c2f1fa
KaschaArtem <kascha767611@gmail.com>
2025-10-19 11:18:27 +0300

    Added GetTextFilePath method in Program class

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -2,9 +2,32 @@
 {
     class Program
     {
-        static void main(string[] args)
+        static string? GetTextFilePath()
         {
+            string textsFolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Texts");
+            string[] textFiles = Directory.GetFiles(textsFolderPath, "*.txt");
 
+            if (!textFiles.Any())
+            {
+                Console.WriteLine("No text files found!");
+                return null;
+            }
+
+            Console.WriteLine("Choose text file:");
+            for (int i = 0; i < textFiles.Length; i++)
+                Console.WriteLine($"{i + 1}) {Path.GetFileNameWithoutExtension(textFiles[i])}");
+
+            int fileChoice;
+            while (!int.TryParse(Console.ReadLine(), out fileChoice) || fileChoice < 1 || fileChoice > textFiles.Length)
+                Console.WriteLine("Wrong input!");
+
+            return textFiles[fileChoice - 1];
+        }
+
+        static void Main(string[] args)
+        {
+            string? inputFilePath = GetTextFilePath();
+            if (inputFilePath == null) { return; }
         }
     }
 }

af18939d7b83393c9d8806b97c77f567bdc0ebe0
KaschaArtem <kascha767611@gmail.com>
2025-10-27 15:23:52 +0300

    Added option choise for text

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -31,16 +31,84 @@ namespace lab3
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
             string? inputFilePath = GetTextFilePath();
-            if (inputFilePath == null) { return; }
+            if (inputFilePath == null) return;
 
             TextParser textParser = new TextParser();
             Text text = textParser.ParseTextFile(inputFilePath);
 
-            text.Print();
-            Console.WriteLine("\n\n\n");
-            text.DeleteStopWords();
+            int optionchoise;
+            while (true)
+            {
+                Console.Write("\n\nChoose option:\n" +
+                    "0. Exit\n" +
+                    "1. Output sentences by increasing of words amount\n" +
+                    "2. Output sentences by increasing of length\n" +
+                    "3. Find unic words in interrogative sentences by input length\n" +
+                    "4. Delete all words with first char is consonant by input length\n" +
+                    "5. Replace words with input substring by input length in choosen sentence\n" +
+                    "6. Delete all stop words in text\n" +
+                    "7. Save text as XML\n"
+                );
+                while (!int.TryParse(Console.ReadLine(), out optionchoise) || optionchoise < 0 || optionchoise > 7)
+                    Console.WriteLine("Wrong input!");
+                Console.WriteLine();
 
-            text.SaveAsXML();
+                int sentenceChoise;
+                int wordLength;
+                string? substring;
+                switch (optionchoise)
+                {
+                    case 0:
+                        return;
+                    case 1:
+                        text.PrintByWordsIncreasing();
+                        break;
+                    case 2:
+                        text.PrintByLengthIncreasing();
+                        break;
+                    case 3:
+                        Console.Write("Input word length: ");
+                        while (!int.TryParse(Console.ReadLine(), out wordLength) || wordLength < 1)
+                            Console.WriteLine("Wrong input!");
+
+                        Console.WriteLine();
+                        text.GetUnicWordsByLength(wordLength);
+                        break;
+                    case 4:
+                        Console.Write("Input word length: ");
+                        while (!int.TryParse(Console.ReadLine(), out wordLength) || wordLength < 1)
+                            Console.WriteLine("Wrong input!");
+
+                        text.DeleteWordsByLength(wordLength);
+                        break;
+                    case 5:
+                        text.PrintSentences();
+                        Console.WriteLine();
+
+                        Console.Write("Choose sentence: ");
+                        while (!int.TryParse(Console.ReadLine(), out sentenceChoise) || sentenceChoise < 1 || sentenceChoise > text.Sentences.Count)
+                            Console.WriteLine("Wrong input!");
+
+                        Console.Write("Input word length: ");
+                        while (!int.TryParse(Console.ReadLine(), out wordLength) || wordLength < 1)
+                            Console.WriteLine("Wrong input!");
+
+                        Console.Write("Input substring: ");
+                        substring = Console.ReadLine();
+                        if (substring == null) substring = "";
+
+                        Console.WriteLine();
+                        text.ReplaceWordsByLength(sentenceChoise - 1, wordLength, substring);
+                        break;
+                    case 6:
+                        text.DeleteStopWords();
+                        break;
+                    case 7:
+                        text.SaveAsXML();
+                        Console.WriteLine("Saved at Texts folder!");
+                        break;
+                }
+            }
         }
     }
 }

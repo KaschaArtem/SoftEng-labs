e73f4a47e5b586a913360bbc50682ba10877d815
KaschaArtem <kascha767611@gmail.com>
2025-11-22 19:38:16 +0300

    Restructure

---
+++ lab4/aircrafts/PassengerAircraft.cs
@@ -0,0 +1,27 @@
+﻿public class PassengerAircraft : Aircraft
+{
+    public int PassengerCapacity { get; set; }
+
+    public PassengerAircraft() { }
+
+    public PassengerAircraft(string model, string aircraftType, int flightRange, float fuelConsumption, int passengerCapacity)
+        : base(model, aircraftType, flightRange, fuelConsumption)
+    {
+        PassengerCapacity = passengerCapacity;
+    }
+
+    public override Aircraft Create()
+    {
+        ReadBaseFields();
+
+        Console.Write("Enter passenger capacity: ");
+        PassengerCapacity = int.Parse(Console.ReadLine()!);
+
+        return this;
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() + $", Passengers: {PassengerCapacity}";
+    }
+}
